d4ca3c74771c6b1c0c958f672df1262d83f52424
James Bremner <james@ravenspoint.com>
2021-05-22 07:56:06 -0400

    doxy update

--- search/functions_3.js
+++ search/functions_3.js
@@ -3,7 +3,7 @@ var searchData=
   ['defaultextension_240',['defaultExtension',['../classwex_1_1filebox.html#ad144dcdcfb6611c4ebe3d08e6a9f6702',1,'wex::filebox']]],
   ['delete_5flist_241',['delete_list',['../classwex_1_1gui.html#a3c71c7f326fd75958ab9076888c962d4',1,'wex::gui']]],
   ['devicecontrolstring_242',['DeviceControlString',['../classwex_1_1com.html#ad2f77e8a2a54e45c037eb34a69f61dae',1,'wex::com']]],
-  ['draw_243',['draw',['../classwex_1_1button.html#a1ba012afb6ef2e5318a03040f795a734',1,'wex::button']]],
+  ['draw_243',['draw',['../classwex_1_1button.html#a1ba012afb6ef2e5318a03040f795a734',1,'wex::button::draw()'],['../classwex_1_1window2file.html#a87135781a1541fc8512dd8a90d9c1321',1,'wex::window2file::draw()']]],
   ['drop_244',['drop',['../classwex_1_1eventhandler.html#a2b5d00e3a3307a55d6a9b870194e2464',1,'wex::eventhandler']]],
   ['dropstart_245',['dropStart',['../classwex_1_1eventhandler.html#acad487ae9df0a44acb6f1a8f231a9455',1,'wex::eventhandler']]]
 ];
