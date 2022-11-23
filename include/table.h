b25ecbdd4617f267e7e6c805a4e08954d9d8dd97
JamesBremner <james@ravenspoint.com>
2022-11-23 12:10:41 -0500

    Read only table of values

--- include/table.h
+++ include/table.h
@@ -1,203 +1,152 @@
-namespace wex {
-
-/// @brief A read only table of values
-
-class table
+namespace wex
 {
-public:
-    table(gui &parent)
-        : myPanel(maker::make()),
-          myParent(&parent),
-          myftracking(false),
-          myRowDisplayCount(35),
-          myRowStart(0)
-    {
-        myPanel.bgcolor(0xFFFFFF);
 
-        registerEventHandlers();
-    }
+    /// @brief A read only table of values
 
-    void move(int x, int y, int w, int h)
+    class table : public gui
     {
-        myPanel.move(x, y, w, h);
-        // myPanel.scrollRange(1500, 1600);
-    }
-
-    void set(const std::vector<std::vector<std::string>> &val)
-    {
-        myContents = val;
-    }
-    void set(const std::vector<std::string> &val, int colcount)
-    {
-        std::vector<std::vector<std::string>> vvs;
-        for (
-            int row = 0;
-            row < val.size() / colcount;
-            row++)
+    public:
+        table(gui *parent)
+            : gui(parent),
+              myRowDisplayCount(25),
+              myRowStart(0)
         {
-            std::vector<std::string> vrow;
-            for (int col = 0; col < colcount; col++)
+
+            registerEventHandlers();
+        }
+
+        /**
+         * @brief Set table values
+         * 
+         * @param val 2D vector of values, cols in rows
+         */
+        void set(const std::vector<std::vector<std::string>> &val)
+        {
+            myContents = val;
+        }
+        /**
+         * @brief Set table values
+         * 
+         * @param val     1D vector of values, cols in row
+         * @param colcount count of columns in a row
+         */
+        void set(const std::vector<std::string> &val, int colcount)
+        {
+            std::vector<std::vector<std::string>> vvs;
+            for (
+                int row = 0;
+                row < val.size() / colcount;
+                row++)
             {
-                if (col == 1)
-                    vrow.push_back(
-                        val[row * colcount + col] + " " + val[row * colcount + col + 1]);
-                else if (col == 2)
-                    continue;
-                else
-                    vrow.push_back(val[row * colcount + col]);
+                std::vector<std::string> vrow;
+                for (int col = 0; col < colcount; col++)
+                {
+                    if (col == 1)
+                        vrow.push_back(
+                            val[row * colcount + col] + " " + val[row * colcount + col + 1]);
+                    else if (col == 2)
+                        continue;
+                    else
+                        vrow.push_back(val[row * colcount + col]);
+                }
+                vvs.push_back(vrow);
             }
-            vvs.push_back(vrow);
+            set(vvs);
         }
-        set(vvs);
-    }
 
-    void show(bool f = true)
-    {
-        if (f)
+        /// @brief Move display window
+        /// @param i increment for row to display at top of window
+        void rowInc(int i)
         {
-            myPanel.move(10, 10, 1500, 800);
-            myPanel.show();
-            // SetCapture(myPanel.handle());
+            myRowStart += i;
+            if (myRowStart < 0)
+                myRowStart = 0;
         }
-        else
+
+    private:
+        int myColCount;
+        std::vector<std::vector<std::string>> myContents;
+        std::vector<int> myRowID;
+        int myRowDisplayCount;
+        int myRowStart;
+
+        void draw(shapes &S)
         {
-            myPanel.show(false);
-            ReleaseCapture();
-        }
-    }
+            myRowID.clear();
 
-private:
-    gui &myPanel;
-    gui *myParent;
-    int myColCount;
-    std::vector<std::vector<std::string>> myContents;
-    bool myftracking;
-    std::vector<int> myRowID;
-    int myRowDisplayCount;
-    int myRowStart;
+            if (!myContents.size())
+                return;
 
-    void draw(shapes &S)
-    {
-        if (!myContents.size())
-            return;
-
-        int colCount = myContents[0].size();
-        int colWidth = myPanel.size()[0] / colCount;
-        // int rowCount = myRowDisplayCount;
-        // if (myRowStart + rowCount > myContents.size())
-        //     rowCount = myContents.size() - myRowStart;
-        // std::cout << "row " << myRowStart << " to " << myRowStart + rowCount - 1
-        //           << " from " << myContents.size() << "\n";
-        for (int kr = myRowStart; kr < myContents.size(); kr++)
-        // for (int kr = myRowStart; kr <25; kr++)
-        {
-            if (myContents[kr].size() > colCount)
-                throw std::runtime_error(
-                    "bad col count");
-
-            for (int kc = 0; kc < colCount; kc++)
+            int colCount = myContents[0].size();
+            int colWidth = size()[0] / (colCount - 1);
+            int rowStop;
+            if (myContents.size() < 20)
             {
-                auto &val = myContents[kr][kc];
-                int x, w;
-                if (kc == 0)
-                {
-                    x = 0;
-                    w = 50;
+                myRowStart = 0;
+                rowStop = myContents.size();
+            }
+            else
+            {
+                if (myRowStart > myContents.size() - 20)
+                    myRowStart = myContents.size() - 20;
+                rowStop = myRowStart + 30;
+                if (rowStop > myContents.size())
+                    rowStop = myContents.size();
+            }
+            // std::cout << "row " << myRowStart << " to " << myRowStart + rowStop - 1
+            //           << " from " << myContents.size() << "\n";
+            for (int kr = myRowStart; kr < rowStop; kr++)
+            {
+                if (myContents[kr].size() > colCount)
+                    throw std::runtime_error(
+                        "bad col count");
 
-                    // save the database ID of the row
-                    // so it can be sent if the row is double clicked
-                    myRowID.push_back(atoi(val.c_str()));
-                }
-                else
+                for (int kc = 0; kc < colCount; kc++)
                 {
-                    x = 50 + (kc - 1) * colWidth;
-                    w = colWidth + 5;
+                    auto &val = myContents[kr][kc];
+                    int x, w;
+                    if (kc == 0)
+                    {
+                        x = 0;
+                        w = 50;
+
+                        // save the database ID of the row
+                        // so it can be sent if the row is double clicked
+                        // assumes first column contains the db id
+                        myRowID.push_back(atoi(val.c_str()));
+                    }
+                    else
+                    {
+                        x = 50 + (kc - 1) * colWidth;
+                        w = colWidth;
+                    }
+                    S.text(
+                        val,
+                        {x, (kr - myRowStart) * 20,
+                         w - 5, 20});
+                    S.line({x + w - 1, (kr - myRowStart) * 20,
+                            x + w - 1, (kr - myRowStart + 1) * 20});
                 }
-                S.text(
-                    val,
-                    {x, (kr - myRowStart) * 20,
-                     w, 20});
             }
         }
-    }
 
         void registerEventHandlers()
         {
-            myPanel.events().draw(
+            events().draw(
                 [&](PAINTSTRUCT &ps)
                 {
                     wex::shapes S(ps);
                     draw(S);
                 });
 
-            myPanel.events().mouseMove(
-                [this](wex::sMouse &m)
-                {
-                    // std::cout << "move " << myftracking << "\n";
-                    if (myftracking)
-                        return;
-
-                    // first mouse movement in slider
-                    // start tracking mouse movement
-                    myftracking = true;
-
-                    // generate event when mouse leaves
-                    TRACKMOUSEEVENT s;
-                    s.cbSize = sizeof(s);
-                    s.hwndTrack = myPanel.handle();
-                    s.dwFlags = TME_LEAVE;
-                    TrackMouseEvent(&s);
-
-                    // SetCapture(myPanel.handle());
-
-                    // run mouse enter event handler
-                    myPanel.events().onMouseEnter();
-                });
-            myPanel.events().mouseEnter(
+            events().click(
                 [this]
                 {
-                    // std::cout << "table mouse enter\n";
-                });
-            myPanel.events().mouseLeave(
-                [this]
-                {
-                    // std::cout << "table mouse left\n";
-                    myftracking = false;
-                    ReleaseCapture();
-                });
-            myPanel.events().clickDouble(
-                [this]
-                {
-                    int row = myPanel.getMouseStatus().y / 20
-                        + myRowStart;
-                    int id = -1;
-                    if (row < myRowID.size())
-                        id = myRowID[row];
-                    // std::cout << "DClick " << id << "\n";
                     PostMessageA(
                         myParent->handle(),
                         wex::eventMsgID::asyncReadComplete,
-                        id,
+                        myRowID[getMouseStatus().y / 20],
                         0);
-                });
-            myPanel.events().keydown(
-                [this](int c)
-                {
-                    std::cout << "keydown " << c << "\n";
-                    switch (c)
-                    {
-                    case 40:
-                        myRowStart += myRowDisplayCount;
-                        break;
-                    case 38:
-                        myRowStart -= myRowDisplayCount;
-                        if (myRowStart < 0)
-                            myRowStart = 0;
-                        break;
-                    default:
-                        return;
-                    }
-                    myPanel.update();
                 });
         }
     };
